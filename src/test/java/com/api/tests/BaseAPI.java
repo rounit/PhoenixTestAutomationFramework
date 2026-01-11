981695711ac5a2a115bd7d5d4c9612fa5830ec0a
Rounit Sharma <rounit19@gmail.com>
2026-01-11 12:31:17 +0530

    Updated

--- src/test/java/com/api/tests/BaseAPI.java
+++ src/test/java/com/api/tests/BaseAPI.java
@@ -3,19 +3,16 @@ package com.api.tests;
 import org.testng.annotations.BeforeMethod;
 
 import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
 
 public class BaseAPI 
 {
 
 	    @BeforeMethod(alwaysRun = true)
 	    public void resetRestAssured() {
-	    	RestAssured.reset();
-
-	        
+	   
 	        RestAssured.requestSpecification = null;
-	        RestAssured.responseSpecification = null;
-
-	        RestAssured.defaultParser = null;
+	        RestAssured.defaultParser = Parser.JSON;
 	    }
 	}
 
