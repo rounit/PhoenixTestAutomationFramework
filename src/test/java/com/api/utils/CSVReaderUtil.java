928c4e8358795c6b4af3d1df619a18255869816f
Rounit Sharma <rounit19@gmail.com>
2026-01-28 14:32:23 +0530

    Created CSVReaderUtil class to read CSV file

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -11,24 +11,27 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CSVReaderUtil {
+	
+	
 
 	private CSVReaderUtil() {
 
 	}
 
-	public static Iterator<UserBean> loadCSV(String pathOfCSVFile) {
+	public static <T> Iterator<T> loadCSV(String pathOfCSVFile,Class<T> bean) {
 
 		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
 		InputStreamReader isr = new InputStreamReader(is);
 		CSVReader csvReader = new CSVReader(isr);
+		
 
-		CsvToBean<UserBean> csvToBean = new CsvToBeanBuilder(csvReader)
-				.withType(UserBean.class)
+		CsvToBean<T> csvToBean = new CsvToBeanBuilder(csvReader)
+				.withType(bean)
 				.withIgnoreEmptyLine(true)
 				.build();
 
-		List<UserBean> userList = csvToBean.parse();
-		return userList.iterator();
+		List<T> list = csvToBean.parse();
+		return list.iterator();
 
 	}
 
