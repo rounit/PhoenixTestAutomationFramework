ce34a67db19455d170d269a4f6e2a2356a7fab04
Rounit Sharma <rounit19@gmail.com>
2026-02-17 17:29:51 +0530

    Completed DataDrivenTest for Create Job API with DB Test Data

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -14,6 +14,7 @@ import com.api.utils.CreateJobBeanMapper;
 import com.api.utils.ExcelReaderUtil;
 import com.api.utils.FakerDataGenerator;
 import com.api.utils.JsonReaderUtil;
+import com.database.dao.CreateJobPayloadDataDao;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 
@@ -90,6 +91,23 @@ public class DataProviderUtils
 		
 		return payloadList.iterator();
 		
+	}
+	
+	@DataProvider(name="CreateJobAPIDBDataProvider",parallel=true)
+	public static Iterator<CreateJobPayload> CreateJobAPIDBDataProvider() 
+	{
+		List<CreateJobBean> beanList=CreateJobPayloadDataDao.getCreateJobPayloadData();
+		List<CreateJobPayload> payloadList = new ArrayList<CreateJobPayload>();
+		
+		for(CreateJobBean createJobBean : beanList)
+		{
+			CreateJobPayload payload = CreateJobBeanMapper.mapper(createJobBean);
+			payloadList.add(payload);
+		}
+		
+		return payloadList.iterator();
+		
+
 	}
 
 }
